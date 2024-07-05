1ca5babff6704652f429e4db77682d709aee0f77
JUN HYUK KWON <sojourn8th@naver.com>
2024-07-05 22:17:35 +0900

    java123

--- java123/src/main/java/com/example/java123/BoardController.java
+++ java123/src/main/java/com/example/java123/BoardController.java
@@ -1,5 +1,23 @@
 package com.example.java123;
 
-public class BoardController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+public class BoardController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@PostMapping("/index")
+	public String indexdata() {
+		return "index";
+	
+	}
+	
+	
 }
---
+++ java123/src/main/java/com/example/java123/MainService.java
@@ -0,0 +1,15 @@
+package com.example.java123;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainService {
+
+	public void servicecreate(String ti, String con) {
+		System.out.println(ti);
+		System.out.println(con);
+		
+	}
+
+
+}
